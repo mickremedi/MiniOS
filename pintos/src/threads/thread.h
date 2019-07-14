b54c65c47d4e4c43b9f8f77a3f14c28cffcdc0df
mickremedi <mickremedi@gmail.com>
2019-07-14 18:41:20 +0000

    Formatted synch and added comments for tasks

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -4,16 +4,15 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
-#include "threads/synch.h"
 #include "threads/fixed-point.h"
+#include "threads/synch.h"
 
 /* States in a thread's life cycle. */
-enum thread_status
-{
-  THREAD_RUNNING, /* Running thread. */
-  THREAD_READY,   /* Not running but ready to run. */
-  THREAD_BLOCKED, /* Waiting for an event to trigger. */
-  THREAD_DYING    /* About to be destroyed. */
+enum thread_status {
+    THREAD_RUNNING, /* Running thread. */
+    THREAD_READY,   /* Not running but ready to run. */
+    THREAD_BLOCKED, /* Waiting for an event to trigger. */
+    THREAD_DYING    /* About to be destroyed. */
 };
 
 /* Thread identifier type.
@@ -82,28 +81,28 @@ typedef int tid_t;
    only because they are mutually exclusive: only a thread in the
    ready state is on the run queue, whereas only a thread in the
    blocked state is on a semaphore wait list. */
-struct thread
-{
-  /* Owned by thread.c. */
-  tid_t tid;                 /* Thread identifier. */
-  enum thread_status status; /* Thread state. */
-  char name[16];             /* Name (for debugging purposes). */
-  uint8_t *stack;            /* Saved stack pointer. */
-  int priority;              /* Priority. */
-  struct list_elem allelem;  /* List element for all threads list. */
-  int64_t wake_up_tick;      /* Tick time for thread to wake up */
-  struct list_elem nap_elem; /* List element for nap room list */
+struct thread {
+    /* Owned by thread.c. */
+    tid_t tid;                 /* Thread identifier. */
+    enum thread_status status; /* Thread state. */
+    char name[16];             /* Name (for debugging purposes). */
+    uint8_t *stack;            /* Saved stack pointer. */
+    int priority;              /* Priority. */
+    int base_priority;
+    struct list_elem allelem;  /* List element for all threads list. */
+    int64_t wake_up_tick;      /* Tick time for thread to wake up */
+    struct list_elem nap_elem; /* List element for nap room list */
 
-  /* Shared between thread.c and synch.c. */
-  struct list_elem elem; /* List element. */
+    /* Shared between thread.c and synch.c. */
+    struct list_elem elem; /* List element. */
 
 #ifdef USERPROG
-  /* Owned by userprog/process.c. */
-  uint32_t *pagedir; /* Page directory. */
+    /* Owned by userprog/process.c. */
+    uint32_t *pagedir; /* Page directory. */
 #endif
 
-  /* Owned by thread.c. */
-  unsigned magic; /* Detects stack overflow. */
+    /* Owned by thread.c. */
+    unsigned magic; /* Detects stack overflow. */
 };
 
 /* If false (default), use round-robin scheduler.
