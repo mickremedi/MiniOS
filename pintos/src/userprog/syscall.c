13918c09a305ed24c365db2309c738a0c435c38e
mickremedi <mickremedi@gmail.com>
2019-07-25 01:32:15 +0000

    Implemented practice

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -1,11 +1,11 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
-#include "threads/interrupt.h"
-#include "threads/thread.h"
+#include "devices/shutdown.h"
 #include "filesys/file.h"
 #include "filesys/filesys.h"
-
+#include "threads/interrupt.h"
+#include "threads/thread.h"
 
 struct lock io_lock;
 static void syscall_handler(struct intr_frame*);
@@ -24,55 +24,52 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
         printf("%s: exit(%d)\n", thread_name, args[1]);
         thread_exit();
     }
-    
-    if(args[0] == SYS_CREATE){
+
+    if (args[0] == SYS_CREATE) {
         lock_acquire(&io_lock);
-        const char* file_name = (const char *) args[1];
-        unsigned size = (unsigned) args[2];
-        f->eax = filesys_create(file_name, size); 
+        const char* file_name = (const char*)args[1];
+        unsigned size = (unsigned)args[2];
+        f->eax = filesys_create(file_name, size);
         lock_release(&io_lock);
     }
-    
-    if(args[0] == SYS_REMOVE){
+
+    if (args[0] == SYS_REMOVE) {
         lock_acquire(&io_lock);
-        const char* file_name = (const char*) args[1];
+        const char* file_name = (const char*)args[1];
         f->eax = filesys_remove(file_name);
         lock_release(&io_lock);
     }
 
-    if(args[0] == SYS_WRITE){
+    if (args[0] == SYS_WRITE) {
         lock_acquire(&io_lock);
-        int fd = (int) args[1];
-        char* buffer = (char *) args[2];
-        int size = (int) args[3];
-        if (fd == 1){
-            while(size >= 100){
-                putbuf(buffer,100);
+        int fd = (int)args[1];
+        char* buffer = (char*)args[2];
+        int size = (int)args[3];
+        if (fd == 1) {
+            while (size >= 100) {
+                putbuf(buffer, 100);
                 buffer += 100;
                 size -= 100;
             }
             putbuf(buffer, size);
-            f-> eax = (int) args[3];
-        }
-        else{
+            f->eax = (int)args[3];
+        } else {
             // f->eax = file_write(fd, buffer, size);
         }
-    
+
         lock_release(&io_lock);
-        
     }
 
-    // if (args[0] == SYS_PRACTICE) {
-    //     char* endptr;
-    //     int i = strtol(args[1], endptr, 10);
-    //     f->eax = i;
-    //     return;
-    // }
+    if (args[0] == SYS_PRACTICE) {
+        int i = (int)args[1];
+        f->eax = i + 1;
+        return;
+    }
 
-    // if (args[0] == SYS_HALT) {
-    //     shutdown_power_off();
-    //     NOT_REACHED();
-    // }
+    if (args[0] == SYS_HALT) {
+        shutdown_power_off();
+        NOT_REACHED();
+    }
 
     // if (args[0] == SYS_EXEC) {
     //     char* file_name = args[1];
